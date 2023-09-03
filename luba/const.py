65c9a69c25a30a86dd12f39b19843332c46913c0
Michael Arthur <michael.arthur@gameglass.gg>
2023-09-04 10:17:52 +1200

    start of HA integration for luba

---
+++ luba/const.py
@@ -0,0 +1,7 @@
+"""Constants for the Mammotion Luba integration."""
+
+DOMAIN = "luba"
+
+# TODO Update with your own urls
+OAUTH2_AUTHORIZE = "https://www.example.com/auth/authorize"
+OAUTH2_TOKEN = "https://www.example.com/auth/token"
