4655b17d7d8d6d9b45b32eb3b2568dc2ca28e27e
jLynx <admin@jlynx.net>
2024-05-20 19:53:45 +1200

    Updated to 1 minute

--- custom_components/mammotion/const.py
+++ custom_components/mammotion/const.py
@@ -8,4 +8,4 @@ DOMAIN: Final = "mammotion"
 DEFAULT_RETRY_COUNT = 3
 CONF_RETRY_COUNT = "retry_count"
 LOGGER: Final = logging.getLogger(__package__)
-SCAN_INTERVAL = timedelta(seconds=30)
+SCAN_INTERVAL = timedelta(minutes=1)
