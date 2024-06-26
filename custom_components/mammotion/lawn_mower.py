fa899a422078823b5f3b1ea9f57b1e317a850cab
jLynx <karter61@gmail.com>
2024-06-26 22:27:14 +1200

    updated serial number

--- custom_components/mammotion/lawn_mower.py
+++ custom_components/mammotion/lawn_mower.py
@@ -75,7 +75,7 @@ class MammotionLawnMowerEntity(MammotionBaseEntity, LawnMowerEntity):
         self._attr_device_info = DeviceInfo(
             identifiers={(DOMAIN, device_name)},
             manufacturer="Mammotion",
-            serial_number=coordinator.device.luba_msg.net.toapp_wifi_iot_status.productkey,
+            serial_number=device_name.split('-', 1)[-1],
             # sw_version=self.mower_data.net.toapp_devinfo_resp.resp_ids[0].info,
             # sw_version=coordinator.device.luba_msg.net.toapp_devinfo_resp.resp_ids[0].info if coordinator.device.luba_msg.net.toapp_devinfo_resp.resp_ids else "Loading...",
             # sw_version=coordinator.device.luba_msg.net.toapp_devinfo_resp.resp_ids.get(0, {}).get('info', "Loading..."),
