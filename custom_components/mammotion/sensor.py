f8cb038e8e98d101398b426a3ba343969270d755
Josef Zweck <24647999+zweckj@users.noreply.github.com>
2024-07-11 10:21:27 +0200

    Further cleanup (#38)
    * further cleanup
    
    * fix not

--- custom_components/mammotion/sensor.py
+++ custom_components/mammotion/sensor.py
@@ -4,7 +4,7 @@ from collections.abc import Callable
 from dataclasses import dataclass
 
 from pyluba.data.model.enums import RTKStatus
-from pyluba.proto.luba_msg import LubaMsg
+from pyluba.proto.luba_msg import ReportInfoData
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -26,7 +26,7 @@ from .entity import MammotionBaseEntity
 class MammotionSensorEntityDescription(SensorEntityDescription):
     """Describes Mammotion sensor entity."""
 
-    value_fn: Callable[[LubaMsg], StateType]
+    value_fn: Callable[[ReportInfoData], StateType]
 
 
 SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
@@ -35,103 +35,92 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.BATTERY,
         native_unit_of_measurement=PERCENTAGE,
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.dev.battery_val,
+        value_fn=lambda mower_data: mower_data.dev.battery_val,
     ),
     MammotionSensorEntityDescription(
         key="ble_rssi",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
         native_unit_of_measurement="dBm",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.connect.ble_rssi,
+        value_fn=lambda mower_data: mower_data.connect.ble_rssi,
     ),
     MammotionSensorEntityDescription(
         key="wifi_rssi",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
         native_unit_of_measurement="dBm",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.connect.wifi_rssi,
+        value_fn=lambda mower_data: mower_data.connect.wifi_rssi,
     ),
     MammotionSensorEntityDescription(
         key="gps_stars",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement=None,
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.rtk.gps_stars,
+        value_fn=lambda mower_data: mower_data.rtk.gps_stars,
     ),
     MammotionSensorEntityDescription(
         key="blade_height",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement="mm",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.knife_height,
+        value_fn=lambda mower_data: mower_data.work.knife_height,
     ),
     MammotionSensorEntityDescription(
         key="area",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement="m²",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.area & 65535,
+        value_fn=lambda mower_data: mower_data.work.area & 65535,
     ),
     MammotionSensorEntityDescription(
         key="mowing_speed",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SPEED,
         native_unit_of_measurement="m/s",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.man_run_speed
-        / 100,
+        value_fn=lambda mower_data: mower_data.work.man_run_speed / 100,
     ),
     MammotionSensorEntityDescription(
         key="progress",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement=PERCENTAGE,
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.area >> 16,
+        value_fn=lambda mower_data: mower_data.work.area >> 16,
     ),
     MammotionSensorEntityDescription(
         key="total_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
         native_unit_of_measurement="min",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.progress
-        & 65535,
+        value_fn=lambda mower_data: mower_data.work.progress & 65535,
     ),
     MammotionSensorEntityDescription(
         key="elapsed_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
         native_unit_of_measurement="min",
-        value_fn=lambda mower_data: (
-            mower_data.sys.toapp_report_data.work.progress & 65535
-        )
-        - (mower_data.sys.toapp_report_data.work.progress >> 16),
+        value_fn=lambda mower_data: (mower_data.work.progress & 65535)
+        - (mower_data.work.progress >> 16),
     ),
     MammotionSensorEntityDescription(
         key="left_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
         native_unit_of_measurement="min",
-        value_fn=lambda mower_data: mower_data.sys.toapp_report_data.work.progress
-        >> 16,
+        value_fn=lambda mower_data: mower_data.work.progress >> 16,
     ),
     MammotionSensorEntityDescription(
         key="l1_satellites",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement=None,
-        value_fn=lambda mower_data: (
-            mower_data.sys.toapp_report_data.rtk.co_view_stars >> 0
-        )
-        & 255,
+        value_fn=lambda mower_data: (mower_data.rtk.co_view_stars >> 0) & 255,
     ),
     MammotionSensorEntityDescription(
         key="l2_satellites",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
         native_unit_of_measurement=None,
-        value_fn=lambda mower_data: (
-            mower_data.sys.toapp_report_data.rtk.co_view_stars >> 8
-        )
-        & 255,
+        value_fn=lambda mower_data: (mower_data.rtk.co_view_stars >> 8) & 255,
     ),
     MammotionSensorEntityDescription(
         key="position_mode",
@@ -139,7 +128,7 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         device_class=SensorDeviceClass.ENUM,
         native_unit_of_measurement=None,
         value_fn=lambda mower_data: str(
-            RTKStatus.from_value(mower_data.sys.toapp_report_data.rtk.status)
+            RTKStatus.from_value(mower_data.rtk.status)
         ),  # Note: This will not work for Luba2 & Yuka. Only for Luba1
     ),
     # ToDo: We still need to add the following.
@@ -186,4 +175,6 @@ class MammotionSensorEntity(MammotionBaseEntity, SensorEntity):
     @property
     def native_value(self) -> StateType:
         """Return the state of the sensor."""
-        return self.entity_description.value_fn(self.coordinator.data)
+        return self.entity_description.value_fn(
+            self.coordinator.data.sys.toapp_report_data
+        )
