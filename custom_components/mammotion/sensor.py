4c26dd57ffcad31b38f92c478674fa4ca4e9f876
Michael Arthur <michael.arthur@gameglass.gg>
2024-08-09 09:27:02 +1200

    update measurement units to consts

--- custom_components/mammotion/sensor.py
+++ custom_components/mammotion/sensor.py
@@ -9,7 +9,8 @@ from homeassistant.components.sensor import (
     SensorEntityDescription,
     SensorStateClass,
 )
-from homeassistant.const import PERCENTAGE
+from homeassistant.const import PERCENTAGE, UnitOfLength, AREA_SQUARE_METERS, SIGNAL_STRENGTH_DECIBELS_MILLIWATT, \
+    UnitOfSpeed, UnitOfTime
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import StateType
@@ -41,14 +42,14 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         key="ble_rssi",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
-        native_unit_of_measurement="dBm",
+        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         value_fn=lambda mower_data: mower_data.connect.ble_rssi,
     ),
     MammotionSensorEntityDescription(
         key="wifi_rssi",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SIGNAL_STRENGTH,
-        native_unit_of_measurement="dBm",
+        native_unit_of_measurement=SIGNAL_STRENGTH_DECIBELS_MILLIWATT,
         value_fn=lambda mower_data: mower_data.connect.wifi_rssi,
     ),
     MammotionSensorEntityDescription(
@@ -62,21 +63,21 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         key="blade_height",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
-        native_unit_of_measurement="mm",
+        native_unit_of_measurement=UnitOfLength.MILLIMETERS,
         value_fn=lambda mower_data: mower_data.work.knife_height,
     ),
     MammotionSensorEntityDescription(
         key="area",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=None,
-        native_unit_of_measurement="m²",
+        native_unit_of_measurement=AREA_SQUARE_METERS,
         value_fn=lambda mower_data: mower_data.work.area & 65535,
     ),
     MammotionSensorEntityDescription(
         key="mowing_speed",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.SPEED,
-        native_unit_of_measurement="m/s",
+        native_unit_of_measurement=UnitOfSpeed.METERS_PER_SECOND,
         value_fn=lambda mower_data: mower_data.work.man_run_speed / 100,
     ),
     MammotionSensorEntityDescription(
@@ -90,14 +91,14 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         key="total_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
-        native_unit_of_measurement="min",
+        native_unit_of_measurement=UnitOfTime.MINUTES,
         value_fn=lambda mower_data: mower_data.work.progress & 65535,
     ),
     MammotionSensorEntityDescription(
         key="elapsed_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
-        native_unit_of_measurement="min",
+        native_unit_of_measurement=UnitOfTime.MINUTES,
         value_fn=lambda mower_data: (mower_data.work.progress & 65535)
         - (mower_data.work.progress >> 16),
     ),
@@ -105,7 +106,7 @@ SENSOR_TYPES: tuple[MammotionSensorEntityDescription, ...] = (
         key="left_time",
         state_class=SensorStateClass.MEASUREMENT,
         device_class=SensorDeviceClass.DURATION,
-        native_unit_of_measurement="min",
+        native_unit_of_measurement=UnitOfTime.MINUTES,
         value_fn=lambda mower_data: mower_data.work.progress >> 16,
     ),
     MammotionSensorEntityDescription(
