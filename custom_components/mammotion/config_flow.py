b52c2e157017a15ddd16d6063d8b244bd01e03a6
Michael Arthur <michael.arthur@gameglass.gg>
2024-08-09 09:10:21 +1200

    fix issues with removing and re-adding robots in the config flow

--- custom_components/mammotion/config_flow.py
+++ custom_components/mammotion/config_flow.py
@@ -21,6 +21,8 @@ class MammotionConfigFlow(ConfigFlow, domain=DOMAIN):
     def __init__(self) -> None:
         """Initialize the config flow."""
         self._discovered_device: BLEDevice | None = None
+        self._discovered_devices: dict[str, str] = {}
+
 
     async def async_step_bluetooth(
         self, discovery_info: BluetoothServiceInfo
@@ -74,14 +76,13 @@ class MammotionConfigFlow(ConfigFlow, domain=DOMAIN):
         self, user_input: dict[str, Any] | None = None
     ) -> ConfigFlowResult:
         """Handle the user step to pick discovered device."""
-        discovered_devices: dict[str, str] = {}
 
         if user_input is not None:
             address = user_input[CONF_ADDRESS]
             await self.async_set_unique_id(address, raise_on_progress=False)
             self._abort_if_unique_id_configured()
 
-            name = discovered_devices.get(address)
+            name = self._discovered_devices.get(address)
             if name is None:
                 return self.async_abort(reason="no_longer_present")
 
@@ -95,19 +96,19 @@ class MammotionConfigFlow(ConfigFlow, domain=DOMAIN):
         current_addresses = self._async_current_ids()
         for discovery_info in async_discovered_service_info(self.hass):
             address = discovery_info.address
-            if address in current_addresses or address in discovered_devices:
+            if address in current_addresses or address in self._discovered_devices:
                 continue
 
-            discovered_devices[address] = discovery_info.name
+            self._discovered_devices[address] = discovery_info.name
 
-        if not discovered_devices:
+        if not self._discovered_devices:
             return self.async_abort(reason="no_devices_found")
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema(
                 {
-                    vol.Required(CONF_ADDRESS): vol.In(discovered_devices),
+                    vol.Required(CONF_ADDRESS): vol.In(self._discovered_devices),
                 },
             ),
         )
