d706e23f81a09ea3f8923c27660623e0bb12f2d8
Michael Arthur <michael.arthur@gameglass.gg>
2025-08-01 11:34:06 +1200

    fix issue with areas after firmware update, fix app version in library which fixes firmware check api call

--- custom_components/mammotion/switch.py
+++ custom_components/mammotion/switch.py
@@ -13,7 +13,6 @@ from homeassistant.helpers import entity_registry as er
 from homeassistant.helpers.entity import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.restore_state import RestoreEntity
-from pymammotion.data.model.hash_list import AreaHashNameList
 from pymammotion.utility.device_type import DeviceType
 
 from . import MammotionConfigEntry
@@ -347,20 +346,15 @@ def async_add_area_entities(
 
     switch_entities: list[MammotionConfigAreaSwitchEntity] = []
     areas = list(map(int, coordinator.data.map.area.keys()))
-    area_name_hashes = [area.hash for area in coordinator.data.map.area_name]
-    area_name = coordinator.data.map.area_name
-    new_areas = (set(areas) | set(area_name_hashes)) - added_areas
+    new_areas = set(areas) - added_areas
     if new_areas:
         for area_id in new_areas:
-            existing_name: AreaHashNameList | None = next(
-                (area for area in area_name if str(area.hash) == str(area_id)),
-                None,
-            )
-            name = (
-                existing_name.name
-                if (existing_name and existing_name.name)
-                else f"{area_id}"
-            )
+            name: str = f"{area_id}"
+            area_data = coordinator.data.map.area.get(area_id).data
+            area_frame = area_data[0] if len(area_data) > 0 else None
+            if area_frame is not None and area_frame.area_label.label != "":
+                name = area_frame.area_label.label
+
             base_area_switch_entity = MammotionConfigAreaSwitchEntityDescription(
                 key=f"{area_id}",
                 translation_key="area",
@@ -381,7 +375,7 @@ def async_add_area_entities(
             )
             added_areas.add(area_id)
 
-    old_areas = set(area_name_hashes) - added_areas
+    old_areas = set(areas) - added_areas
     if old_areas:
         async_remove_entities(coordinator, old_areas)
         for area in old_areas:
