833b557edd3a1a8a2311a1210d75364ffa4bfbe7
Michael Arthur <michael.arthur@gameglass.gg>
2025-08-05 08:48:07 +1200

    check if check_versions is none

--- custom_components/mammotion/coordinator.py
+++ custom_components/mammotion/coordinator.py
@@ -807,10 +807,10 @@ class MammotionDeviceVersionUpdateCoordinator(
             ota_info = await device.mammotion_http.get_device_ota_firmware(
                 [device.iot_id]
             )
-            if ota_info is not None:
-           		for check_version in ota_info.data:
-               		if check_version.device_id == device.iot_id:
-                   		device.state.update_check = check_version
+            if check_versions := ota_info.data:
+                for check_version in check_versions:
+                    if check_version.device_id == device.iot_id:
+                        device.state.update_check = check_version
 
         except DeviceOfflineException:
             """Device is offline bluetooth has been attempted."""
